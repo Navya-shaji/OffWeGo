36528f1be07e1ca6e218602b5fdfc941f3e65c1f
Navyacs-crypto <navyacshaji12@gmail.com>
2025-08-08 21:36:30 +0530

    constant routes added to the admin routes

--- Backend/src/framework/routes/Admin/adminRoute.ts
+++ Backend/src/framework/routes/Admin/adminRoute.ts
@@ -14,6 +14,7 @@ import {
 import { verifyTokenAndCheckBlackList } from "../../../adapters/FlowControl/TokenValidationControl";
 import { JwtSevice } from "../../services/jwtService";
 const TokenService = new JwtSevice();
+import { AdminRoutes } from "../Constants/AdminRouteContants";
 
 export class AdminRoute {
   public adminRouter: Router;
@@ -25,87 +26,87 @@ export class AdminRoute {
 
   private setRoutes(): void {
     
-    this.adminRouter.post("/login", (req: Request, res: Response) => {
+    this.adminRouter.post(AdminRoutes.LOGIN, (req: Request, res: Response) => {
       adminController.login(req, res);
     });
     this.adminRouter.use(verifyTokenAndCheckBlackList(TokenService));
 
 
-  this.adminRouter.get('/destinations', (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_DESTINATIONS, (req: Request, res: Response) => {
     destinationController.getAllDestination(req, res);
   });
 
 
-  this.adminRouter.get("/vendors/:email", (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_VENDOR_BY_EMAIL, (req: Request, res: Response) => {
     adminVendorController.getVendorByEmail(req,res)
   });
 
-  this.adminRouter.patch("/vendors/status/:id", (req: Request, res: Response) => {
+  this.adminRouter.patch(AdminRoutes.ADMIN_VENDOR_APPROVAL, (req: Request, res: Response) => {
     adminVendorController.updateVendorApprovalStatus(req,res)
   });
 
-  this.adminRouter.get("/vendors", (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_ALL_VENDORS, (req: Request, res: Response) => {
     adminVendorController.getAllVendors(req, res);
   });
 
-  this.adminRouter.get("/vendors/status/:status", (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_VENDOR_BY_STATUS, (req: Request, res: Response) => {
     adminVendorController.getVendorsByStatus(req, res);
   });
 
-  this.adminRouter.get("/users", (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_ALL_USERS, (req: Request, res: Response) => {
     AdminuserController.getAllUsers(req, res);
   });
 
-  this.adminRouter.patch("/user/status/:id", (req: Request, res: Response) => {
+  this.adminRouter.patch(AdminRoutes.UPDATE_USER_STATUS, (req: Request, res: Response) => {
     AdminuserController.updateStatus(req, res);
   });
 
-  this.adminRouter.post('/create-destination', (req: Request, res: Response) => {
+  this.adminRouter.post(AdminRoutes.CREATE_DESTINATION, (req: Request, res: Response) => {
     destinationController.addDestination(req, res);
   });
 
-  this.adminRouter.put('/edit/:id', (req: Request, res: Response) => {
+  this.adminRouter.put(AdminRoutes.EDIT_DESTINATION, (req: Request, res: Response) => {
     destinationController.editDestinationHandler(req, res);
   });
 
-  this.adminRouter.patch("/vendors/isBlocked/:id", (req: Request, res: Response) => {
+  this.adminRouter.patch(AdminRoutes.BLOCK_UNBLOCK_VENDOR, (req: Request, res: Response) => {
     adminVendorController.blockOrUnblockVendor(req,res)
   });
 
-  this.adminRouter.post("/create-categories", (req: Request, res: Response) => {
+  this.adminRouter.post(AdminRoutes.CREATE_CATEGORY, (req: Request, res: Response) => {
     categoryController.createCategory(req, res);
   });
 
-  this.adminRouter.get("/categories", (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_ALL_CATEGORIES, (req: Request, res: Response) => {
     categoryController.getCategories(req, res);
   });
 
-  this.adminRouter.post("/create-banner", (req: Request, res: Response) => {
+  this.adminRouter.post(AdminRoutes.CREATE_BANNER, (req: Request, res: Response) => {
     bannerController.CreateBanner(req, res);
   });
 
-  this.adminRouter.get("/banner", (req: Request, res: Response) => {
+  this.adminRouter.get(AdminRoutes.GET_ALL_BANNERS, (req: Request, res: Response) => {
     bannerController.getBanners(req, res);
   });
-  this.adminRouter.delete('/destination/:id',(req:Request,res:Response)=>{
+  this.adminRouter.delete(AdminRoutes.DELETE_DESTINATION,(req:Request,res:Response)=>{
     destinationController.deleteDestinationController(req,res)
   })
-  this.adminRouter.put("/category/:id",(req:Request,res:Response)=>{
+  this.adminRouter.put(AdminRoutes.EDIT_CATEGORY,(req:Request,res:Response)=>{
     categoryController.EditCategory(req,res)
   })
-  this.adminRouter.delete("/category/:id",(req:Request,res:Response)=>{
+  this.adminRouter.delete(AdminRoutes.DELETE_CATEGORY,(req:Request,res:Response)=>{
     categoryController.DeleteCategory(req,res)
   })
-  this.adminRouter.put("/banner/:id",(req:Request,res:Response)=>{
+  this.adminRouter.put(AdminRoutes.EDIT_BANNER,(req:Request,res:Response)=>{
     bannerController.EditBanner(req,res)
   })
-  this.adminRouter.delete("/banner/:id",(req:Request,res:Response)=>{
+  this.adminRouter.delete(AdminRoutes.DELETE_BANNER,(req:Request,res:Response)=>{
     bannerController.BannerDelete(req,res)
   })
-  this.adminRouter.post("/create-subscription",(req:Request,res:Response)=>{
+  this.adminRouter.post(AdminRoutes.CREATE_SUBSCRIPTION,(req:Request,res:Response)=>{
     subscriptionController.createSubscription(req,res)
   })
-  this.adminRouter.get("/subscriptions",(req:Request,res:Response)=>{
+  this.adminRouter.get(AdminRoutes.GET_ALL_SUBSCRIPTIONS,(req:Request,res:Response)=>{
     subscriptionController.getAllSubscriptions(req,res)
   })
 }
